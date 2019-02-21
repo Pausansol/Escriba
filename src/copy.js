645ffd48e63dbc8a5eabdd45ae057b252895ff90
Pausansol <pausansol@gmail.com>
2019-02-21 22:45:44 +0100

    code refine

--- src/copy.js
+++ src/copy.js
@@ -15,16 +15,11 @@ export function copySelected (selectedOverrideId, context){
 				var property = override.property
 				var settingName = [`settings${property}`]
 				Settings.setSessionVariable(settingName, string)
-				
-				// googleAnalytics(context, "Escriba Success", "Copy done", string);
+				googleAnalytics(context, "Escriba Success", "Copy done", string);
 			}
 	    })
 	})
 }
-// para hacer varios overrides
-// var pedro = 'dede';
-// var settings = [`settings${pedro}`];
-// console.log(settings)
 
 export default function(context) {
   const selectedOverrides = context.document.documentData().selectedOverrides();
@@ -34,17 +29,17 @@ export default function(context) {
 			var element = tokens[tokens.length-1] 
 			var tokensSplit = element.split('_')
 			var selectedOverrideId = tokensSplit[tokensSplit.length-2]
-			// googleAnalytics(context, "Escriba Initiated", "Copy selected", selectedOverrideId);
+			googleAnalytics(context, "Escriba Initiated", "Copy selected", selectedOverrideId);
 			copySelected(selectedOverrideId, context)
 			})	
 	}
 	if (selectedOverrides.length < 1){
 		UI.message('⚠️ Select one Symbol override to copy ⚠️')
-		// googleAnalytics(context, "Escriba UI", "No override selected to copy");	
+		googleAnalytics(context, "Escriba UI", "No override selected to copy");	
 	}
 	if (selectedOverrides.length > 1){
 		UI.message('⚠️ You have selected more than one override, select only one Symbol override to copy ⚠️')
-		// googleAnalytics(context, "Escriba UI", "More than one override selected to copy");
+		googleAnalytics(context, "Escriba UI", "More than one override selected to copy");
 	}
 }
 
